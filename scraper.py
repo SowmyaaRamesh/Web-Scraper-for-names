d51a90762416cea390160c8ab97945ef5907df9a
Roboramv2 <sriramkraja@gmail.com>
2021-08-08 13:50:48 +0530

    updated functions

--- scraper.py
+++ scraper.py
@@ -9,17 +9,20 @@ def get_html_text(url):
     html_text = requests.get(url).text
     return html_text
 
-def has_special_char(string):
+def check_and_append(string):
     special_char = list('[@_!#$%^&*()<>?/\|}{~:]0123456789')
     for i in special_char:
         if (string.find(i)>=0):
             print("invalid found: "+string)
+            invalid.append(string)
             return 1
     try:
         string.encode('ascii')
     except:
         print("invalid found: "+string)
+        invalid.append(string)
         return 1
+    girl_names.append(string)
     return 0
 
 def scrape_girl_names():
@@ -28,7 +31,8 @@ def scrape_girl_names():
     soup = BeautifulSoup(html_text, 'lxml')
     names = soup.find_all('span', class_='jsx-1193454341 mr-10')
     for name in names:
-        girl_names.append(name.text)
+        parsed_name = name.text
+        check_and_append(parsed_name)
 
     #Site 2: Anime Girls 
     html_text = get_html_text("https://kidadl.com/articles/top-anime-girl-names-with-meanings")
@@ -36,10 +40,8 @@ def scrape_girl_names():
     names = soup.select('p strong')
     for name in names:
         parsed_name = name.text.split(' ')[1]
-        if has_special_char(parsed_name):
-            invalid.append(parsed_name)
-        else:
-            girl_names.append(parsed_name)
+        check_and_append(parsed_name)
+
 
 scrape_girl_names()
 print(girl_names)
